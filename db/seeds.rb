6148bc2251c14db11b1f90da1f90c3bbc5c9ebcf
Cindy Xie <cindyxie89@gmail.com>
2018-06-24 18:17:20 +0100

    some minor changes to doses form and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
 
-Ingredient.destroy_all
 Cocktail.destroy_all
-Cocktail.create!(name: "Mint Mojito")
-Cocktail.create!(name: "Margarita")
+Cocktail.create!(name: "Mint Mojito", description: "6 Mint Leaves, 1oz lime juice, 2 teaspoon Sugar, 1 1/2oz White Rum, Soda Water")
+Cocktail.create!(name: "Margarita", description: "1oz Cointreau, 2oz Tequila, 1oz Lime Juice")
+Cocktail.create!(name: "White Russian", description: "1 2/3oz Vodka, 2/3oz Coffee Liqeur, 1oz Fresh Cream")
+Cocktail.create!(name: "Gin & Tonic", description: "2oz Gin, 3oz Tonic Water, 1 Lime wedge")
+Cocktail.create!(name: "Aperol Spritz", description: "2oz Prosecco, 1 1/4oz Aperol, Splash of Soda Water")
 
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -24,8 +26,8 @@ end
 
 
 
-doses= ["30ml Rum", "30ml Vodka", "30ml Whiskey", "30ml Tequila"]
+# doses= ["30ml Rum", "30ml Vodka", "30ml Whiskey", "30ml Tequila"]
 
-doses.each do |dose|
-  Dose.create(description: dose)
-end
+# doses.each do |dose|
+#   Dose.create(description: dose)
+# end
